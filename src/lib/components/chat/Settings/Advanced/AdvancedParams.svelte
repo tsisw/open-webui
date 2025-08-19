1f88cb4479babb19ad25fef5f994608138b0c046
atrivedi-tsavoritesi <atrivedi@tsavoritesi.com>
2025-08-19 15:57:32 -0700

    @FIR-904: Make changes to make OPU target as default (#43)
    Co-authored-by: Ashish Trivedi <atrivedi@fpga4.tsavoritesi.net>

--- src/lib/components/chat/Settings/Advanced/AdvancedParams.svelte
+++ src/lib/components/chat/Settings/Advanced/AdvancedParams.svelte
@@ -90,7 +90,27 @@
 			</div>
 		</Tooltip>
 	</div>
-
+	<div class="py-0.5 w-full justify-between">
+		<Tooltip
+			content={$i18n.t('Configure backend Ollama queries to go to Native (HOST), CPU (FPGA Host), and OPU (FPGA)')}
+			placement="top-start"
+			className="inline-tooltip"
+			>
+			<div class="flex w-full justify-between">
+			<div class="self-center text-xs font-medium">
+					{$i18n.t('Target')}
+			</div>
+				<select
+					class="p-1 px-3 text-xs flex rounded-sm transition shrink-0 outline-hidden bg-white text-black dark:bg-gray-800 dark:text-white"
+					bind:value={params.target}
+				>
+					<option value="opu">{$i18n.t('OPU')}</option>
+					<option value="cpu">{$i18n.t('CPU')}</option>
+					<option value="native">{$i18n.t('Native')}</option>
+				</select>
+			</div>
+		</Tooltip>
+	</div>
 	{#if admin}
 		<div>
 			<Tooltip
