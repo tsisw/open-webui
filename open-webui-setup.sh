8c72d9165ef0305fcfeba1fa465157c731db325a
Ashish Trivedi <atrivedi@fpga3.tsavoritesi.net>
2025-10-10 21:09:48 -0700

    @FIR-1018: Add support to install open-webui and ollama natively The change consists of 1. Detecting the host is qemuarm64 2. Doing ollama installation on the qemuarm guest 3. Doing Open-WebUI install on the qemuarm guest

--- open-webui-setup.sh
+++ open-webui-setup.sh
@@ -8,28 +8,34 @@ then
 source .venv/bin/activate
 else
 pip install uv
-if [ "${TSI_HOSTNAME}" == "fpga1.tsavoritesi.net" ] || [ "${TSI_HOSTNAME}" == "fpga2.tsavoritesi.net" ]  || [ "${TSI_HOSTNAME}" == "fpga3.tsavoritesi.net" ]  || [ "${TSI_HOSTNAME}" == "fpga4.tsavoritesi.net" ];
+if [ "${TSI_HOSTNAME}" == "fpga1.tsavoritesi.net" ] || [ "${TSI_HOSTNAME}" == "fpga2.tsavoritesi.net" ]  || [ "${TSI_HOSTNAME}" == "fpga3.tsavoritesi.net" ]  || [ "${TSI_HOSTNAME}" == "fpga4.tsavoritesi.net" ] || [ "${TSI_HOSTNAME}" == "qemuarm64" ];
 then
 uv venv
 else
 uv venv --python=python3.12
 fi
 source .venv/bin/activate
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | sh
+export NVM_DIR="$HOME/.nvm"
+. "$NVM_DIR/nvm.sh"
 uv pip install -r backend/requirements.txt
-pip install npm
-if [ "${TSI_HOSTNAME}" == "fpga1.tsavoritesi.net" ] || [ "${TSI_HOSTNAME}" == "fpga2.tsavoritesi.net" ]  || [ "${TSI_HOSTNAME}" == "fpga3.tsavoritesi.net" ]  || [ "${TSI_HOSTNAME}" == "fpga4.tsavoritesi.net" ];
-then
-sudo pip install portalocker
-else
-pip install portalocker
-fi
-node -v
-npm -v
 export WEBUI_NAME="TSI WebUI"
 nvm install 20.18.1
 nvm use 20.18.1
+node -v
+npm -v
 pip install --upgrade chromadb
+if [ "${TSI_HOSTNAME}" == "fpga1.tsavoritesi.net" ] || [ "${TSI_HOSTNAME}" == "fpga2.tsavoritesi.net" ]  || [ "${TSI_HOSTNAME}" == "fpga3.tsavoritesi.net" ]  || [ "${TSI_HOSTNAME}" == "fpga4.tsavoritesi.net" ];
+then
+sudo pip install portalocker
+elif [ "${TSI_HOSTNAME}" == "qemuarm64" ]
+then
+export NODE_OPTIONS="--max-old-space-size=2048"
+#curl -fsSL https://ollama.com/install.sh | sh
+pip install portalocker
+else
+pip install portalocker
+fi
 ln -s $(pwd)/build backend/open_webui/frontend
 fi
 black backend/
