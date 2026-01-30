6f2df589585e9d46c96ec90b64f36a862a752808
atrivedi-tsavoritesi <atrivedi@tsavoritesi.com>
2026-01-30 11:14:10 -0800

    @FIR-1321: Make changes to use ssh for non fpga host when connecting to terminal (#113)
    This change does following
    Connects to ssh 192.168.1.200 instead of connecting to terminal via
    Picocom
    
    The test results are as follows
    Connect via Browser to a redirected port in this case I am using 55003
    mapped to 5003 on which /terminal is running so my browser on my laptop
    has
    
    https://fpga4.tsavoritesi.net:55003/terminal
    
    The Container I created is as follows
    docker run --name=ashish_container -p 58443:48443 -p 55003:5003 --cap-add=NET_ADMIN --shm-size=5G --device /dev/net/tun --network=bridge -dit tsi-container-simat42:latest && docker network connect 192bridge ashish_container && docker exec -it ashish_container bash
    
    Co-authored-by: Ashish Trivedi <atrivedi@fpga4.tsavoritesi.net>

--- backend/open_webui/routers/flaskIfc/enter_pico.sh
+++ backend/open_webui/routers/flaskIfc/enter_pico.sh
@@ -1,1 +1,7 @@
-picocom /dev/ttyUSB3 -b 921600
+export TSI_HOSTNAME=$(hostname)
+if [ "${TSI_HOSTNAME}" == "fpga1.tsavoritesi.net" ] || [ "${TSI_HOSTNAME}" == "fpga2.tsavoritesi.net" ]  || [ "${TSI_HOSTNAME}" == "fpga3.tsavoritesi.net" ]  || [ "${TSI_HOSTNAME}" == "fpga4.tsavoritesi.net" ];
+then
+    picocom /dev/ttyUSB3 -b 921600
+else
+    ssh 192.168.1.200
+fi
